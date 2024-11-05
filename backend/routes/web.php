f36ac9e9d44162bdf53a10ceac9a22b5c7efb36b
João Abner <joaoabnerpereira@gmail.com>
2024-11-05 11:38:07 -0300

    Adicionado middleware 'role'  no Kernel e rotas no web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/jobs/create', 'JobController@create')->middleware('role:Recrutador');
+    Route::post('/jobs', 'JobController@store')->middleware('role:Recrutador');
+    Route::get('/jobs', 'JobController@index')->middleware('role:Candidato');
+    Route::post('/applications', 'ApplicationController@store')->middleware('role:Candidato');
+});
