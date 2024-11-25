d0f6facf6683e020f8f15a413fa256c1456fd1c0
João Abner <joaoabnerpereira@gmail.com>
2024-11-25 09:23:52 -0300

    Ajuste na url base da api

--- frontend/src/services/HttpService.js
+++ frontend/src/services/HttpService.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
 const HttpService = axios.create({
-  baseURL: "http://localhost:8001/",
+  baseURL: "http://localhost:8001/api",
   headers: {
     "Content-type": "application/json",
   },
 });
+
+// // Interceptor para adicionar o token Bearer a todas as requisições
+// HttpService.interceptors.request.use(config => {
+//   const token = localStorage.getItem('token'); // Obtém o token do localStorage
+//   if (token) {
+//     config.headers['Authorization'] = `Bearer ${token}`; // Adiciona o token ao cabeçalho
+//   }
+//   return config;
+// }, error => {
+//   return Promise.reject(error);
+// });
 
 export const registerUser = async (userData) => {
   try {
