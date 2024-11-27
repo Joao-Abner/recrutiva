1dad5dcc9a467572c5a930eee617dd9e4d9235d5
João Abner <joaoabnerpereira@gmail.com>
2024-11-27 08:44:03 -0300

    Removed unused Bearer token interceptor from HttpService

--- frontend/src/services/HttpService.js
+++ frontend/src/services/HttpService.js
@@ -7,17 +7,6 @@ const HttpService = axios.create({
   },
 });
 
-// // Interceptor para adicionar o token Bearer a todas as requisições
-// HttpService.interceptors.request.use(config => {
-//   const token = localStorage.getItem('token'); // Obtém o token do localStorage
-//   if (token) {
-//     config.headers['Authorization'] = `Bearer ${token}`; // Adiciona o token ao cabeçalho
-//   }
-//   return config;
-// }, error => {
-//   return Promise.reject(error);
-// });
-
 export const registerUser = async (userData) => {
   try {
     const response = await api.post('/register', userData);
