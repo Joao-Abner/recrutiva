28729661fa781736b45684fd435c5b92e0278935
João Abner <joaoabnerpereira@gmail.com>
2024-11-28 09:39:43 -0300

    Refatorado LoginCandidato.vue e LoginRecrutador.vue para melhorar o tratamento de erros e a consistência da interface do usuário

--- frontend/src/views/LoginCandidato.vue
+++ frontend/src/views/LoginCandidato.vue
@@ -1,60 +1,43 @@
 <template>
-    <div class="container">
+  <div class="container">
     <div class="container_top_page">
-      
       <RouterLink to="/" class="button_sair">
         <ArrowLeft />
-      </RouterLink> 
-      
+      </RouterLink>
+
       <div class="texto_header">
         <h1>Olá, Candidato!</h1>
-      
-      <p>Encontre a sua vaga ideal em empresas comprometidas com o seu futuro.</p>
+
+        <p>Encontre a sua vaga ideal em empresas comprometidas com o seu futuro.</p>
       </div>
     </div>
-  
-      <div class="tabs">
-        <RouterLink to="/loginca" class="tab">JÁ SOU CADASTRADO</RouterLink>
-        <RouterLink to="/cadastro" class="tab active">QUERO ME CADASTRAR</RouterLink>
-      </div>
-  
-      <!-- <div class="box-info">
-        <form @submit.prevent="handleSubmit" 
-        id="registrationForm">
-  
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Digite seu Email" required>
-          </div>
-  
-          <div class="form-group">
-            <label for="senha">Senha</label>
-            <input type="password" name="senha" id="senha" placeholder="Digite sua senha" required>
-          </div>
-  
-          <button type="submit" class="submit-button">EFETUAR LOGIN</button>
-        </form>
-      </div> -->
-      <div class="form-container">
-    <form @submit.prevent="handleSubmit">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" v-model="formData.email" required>
-      </div>
-      <div class="form-group">
-        <label for="password">Senha</label>
-        <input type="password" v-model="formData.password" required>
-      </div>
-      <button type="submit" class="submit-button">EFETUAR LOGIN</button>
-    </form>
-    <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+
+    <div class="tabs">
+      <RouterLink to="/loginca" class="tab">JÁ SOU CADASTRADO</RouterLink>
+      <RouterLink to="/cadastro" class="tab active">QUERO ME CADASTRAR</RouterLink>
+    </div>
+    <div class="form-container">
+      <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" v-model="formData.email" placeholder="Digite seu Email" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Senha</label>
+          <input type="password" v-model="formData.password" placeholder="Digite sua senha" required>
+        </div>
+        <button type="submit" class="submit-button">EFETUAR LOGIN</button>
+      </form>
+      <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+    </div>
+
+    <div class="footer">
+      <RouterLink to="/loginre" class="users-links">
+        Clique para acessar como Recrutador
+      </RouterLink>
+    </div>
   </div>
-  
-      <div class="footer">
-        <p>Problemas com seu cadastro? <a href="#">Clique para contatar o suporte</a></p>
-      </div>
-    </div>
-  </template>
+</template>
 
 <script>
 import { RouterLink } from 'vue-router';
@@ -67,39 +50,38 @@ export default {
   components: {
     ArrowLeft
   },
-    data() {
-        return {
-            formData: {
-                email: '',
-                password: '',
-            },
-            errorMessage: '',
-        };
-    },
-    methods: {
-      ...mapActions(['loginCandidate']), // Mapeia a ação de login do Vuex
-
-      async handleSubmit() {
-        try {
-          await this.loginCandidate(this.formData); // Chama a ação de login
-          console.log('Role:', this.$store.getters.role); // Verifica o papel do usuário
-          if (this.$store.getters.role !== 'candidate') {
-            throw new Error('Usuário não é um candidato');
-          }
-          this.$router.push('/dashcandidato'); // Redireciona após login bem-sucedido
-        } catch (error) {
-          console.error("Erro ao fazer login:", error);
-        alert('Falha ao efetuar login. Verifique suas credenciais.');
-        }
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: '',
       },
+      errorMessage: '',
+    };
+  },
+  methods: {
+    ...mapActions(['loginCandidate']), // Mapeia a ação de login do Vuex
+
+    async handleSubmit() {
+      try {
+        await this.loginCandidate(this.formData); // Chama a ação de login
+        console.log('Role:', this.$store.getters.role); // Verifica o papel do usuário
+        if (this.$store.getters.role !== 'candidate') {
+          throw new Error('Usuário não é um candidato');
+        }
+        this.$router.push('/dashcandidato'); // Redireciona após login bem-sucedido
+      } catch (error) {
+        console.error("Erro ao fazer login:", error);
+        alert('Falha ao efetuar login. Verifique suas credenciais.');
+      }
     },
+  },
 };
 
 </script>
 
 
 <style>
-
 .container {
   max-width: 37.5rem;
   margin: auto;
@@ -116,7 +98,7 @@ export default {
   flex-direction: column;
 }
 
-.texto_header{
+.texto_header {
   display: flex;
   flex-direction: column
 }
@@ -140,7 +122,7 @@ export default {
   border-bottom: .125rem solid #f02424;
 }
 
-.button_sair{
+.button_sair {
   padding: .625rem 1.25rem;
   background-color: #2c3e50;
   cursor: pointer;
@@ -153,6 +135,15 @@ export default {
   padding: 1.25rem;
   background-color: #f9f9f9;
   border-radius: .5rem;
+}
+
+.form-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 }
 
 .form-group {
@@ -172,7 +163,8 @@ label {
   font-weight: bold;
 }
 
-input, select {
+input,
+select {
   padding: .625rem;
   border: .0625rem solid #ddd;
   border-radius: .25rem;
